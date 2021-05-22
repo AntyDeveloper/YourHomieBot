f8f98d253eccbab2eec0704f06e965f36479a9c7
Rekage <59744361+Rekage@users.noreply.github.com>
2021-05-22 17:37:49 +0200

    master

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+module.exports = {
+	name: 'help',
+	description: 'Ping!',
+	execute(message, args) {
+        message.channel.send(`Prefix bota: **"y"**\nWszyscy użytkownicy: ${message.guild.memberCount}`);
+	},
+};
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const { prefix, token } = require('./config.json');
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+client.commands = new Discord.Collection();
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setActivity("z przyjaciółmi", { type: "PLAYING" });
+});
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'ping') {
+		message.channel.send('Pong.');
+    }
+});
+
+client.login(token);
