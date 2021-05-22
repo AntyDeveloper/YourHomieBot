b28f05cb1466c7c266f17d958cd26deb17b93bab
Rekage <59744361+Rekage@users.noreply.github.com>
2021-05-22 18:28:23 +0200

    main

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const { prefix, token } = require('./config.json');
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 client.commands = new Discord.Collection();
 
 
